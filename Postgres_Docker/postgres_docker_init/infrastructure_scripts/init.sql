0936d6d4ca577cfb36479b7d534c0a043a6a7531
*Ezekiel <*ezekieloshin@gmail.com*>
2024-05-17 05:36:54 +0100

    Implemented the requested change by swapping the dataset with that of SuperStore (a proposed e-commerce database)

--- Postgres_Docker/postgres_docker_init/infrastructure_scripts/init.sql
+++ Postgres_Docker/postgres_docker_init/infrastructure_scripts/init.sql
@@ -1,66 +1,66 @@
 -- Create schema if it does not exist
-CREATE SCHEMA IF NOT EXISTS FIT_PRO;
+CREATE SCHEMA IF NOT EXISTS superstore;
 
--- Create and populate PRODUCTS table
-CREATE TABLE IF NOT EXISTS FIT_PRO.PRODUCTS
-(
-    id SERIAL PRIMARY KEY, -- Unique identifier for each product
-    name VARCHAR(255) NOT NULL, -- Name of the product, limit length to 255 characters
-    price NUMERIC(10, 2) NOT NULL -- Price of the product with precision of 10 digits and 2 decimal places
-);
-
--- Import data into PRODUCTS table from a CSV file
-COPY FIT_PRO.PRODUCTS (id, name, price)
-FROM 'C:\Users\HP\Downloads\products.csv' DELIMITER ',' CSV HEADER; -- Importing data from CSV with comma delimiter and header
-
--- Create and populate CUSTOMERS table
-CREATE TABLE IF NOT EXISTS FIT_PRO.CUSTOMERS
-(
-    customer_id UUID PRIMARY KEY, -- Unique identifier for each customer
-    device_id UUID NOT NULL, -- Unique identifier for the device used by the customer
-    location VARCHAR(300), -- Location of the customer, allowing up to 300 characters
-    currency CHAR(3) -- Currency code typically represented with 3 characters
+-- Create CUSTOMERS table
+CREATE TABLE IF NOT EXISTS superstore.customers (
+    customer_id VARCHAR(50) PRIMARY KEY, -- Unique identifier for each customer
+    customer_name VARCHAR(50) NOT NULL, -- Name of the customer
+    segment VARCHAR(50) NOT NULL, -- Customer segment
+    country_region VARCHAR(50) NOT NULL, -- Country or region of the customer
+    city VARCHAR(50) NOT NULL, -- City of the customer
+    state_province VARCHAR(50) NOT NULL, -- State or province of the customer
+    postal_code VARCHAR(50), -- Postal code of the customer
+    region VARCHAR(50) NOT NULL -- Region of the customer
 );
 
 -- Import data into CUSTOMERS table from a CSV file
-COPY FIT_PRO.CUSTOMERS (customer_id, device_id, location, currency)
-FROM 'C:\Users\HP\Downloads\customer.csv' DELIMITER ',' CSV HEADER; -- Importing customer data from CSV file
+COPY superstore.customers (customer_id, customer_name, segment, country_region, city, state_province, postal_code, region)
+FROM 'C:\Users\HP\Desktop\altschool_project\third_semester_repo\Postgres_Docker\postgres_docker_init\data\customers.csv' DELIMITER ',' CSV HEADER; -- Importing customer data from CSV file
+
+-- Create PRODUCTS table
+CREATE TABLE IF NOT EXISTS superstore.products (
+    product_id VARCHAR(50) PRIMARY KEY, -- Unique identifier for each product
+    category VARCHAR(50) NOT NULL, -- Product category
+    sub_category VARCHAR(50) NOT NULL, -- Product sub-category
+    product_name VARCHAR(128) NOT NULL -- Name of the product
+);
+
+-- Import data into PRODUCTS table from a CSV file
+COPY superstore.products (product_id, category, sub_category, product_name)
+FROM 'C:\Users\HP\Desktop\altschool_project\third_semester_repo\Postgres_Docker\postgres_docker_init\data\products.csv' DELIMITER ',' CSV HEADER; -- Importing product data from CSV file
 
 -- Create ORDERS table
-CREATE TABLE IF NOT EXISTS FIT_PRO.ORDERS
-(
-    order_id UUID PRIMARY KEY, -- Unique identifier for each order
-    customer_id UUID REFERENCES FIT_PRO.CUSTOMERS(customer_id), -- Reference to the customer who placed the order
-    status VARCHAR(30) NOT NULL, -- Status of the order, limited to 30 characters
-    checked_out_at TIMESTAMP WITH TIME ZONE -- Timestamp indicating when the order was checked out, with timezone information
+CREATE TABLE IF NOT EXISTS superstore.orders (
+    order_id VARCHAR(50) PRIMARY KEY, -- Unique identifier for each order
+    order_date VARCHAR(50) NOT NULL, -- Order date
+    ship_date VARCHAR(50) NOT NULL, -- Shipping date
+    ship_mode VARCHAR(50) NOT NULL, -- Shipping mode
+    customer_id VARCHAR(50) REFERENCES superstore.customers(customer_id), -- Reference to the customer who placed the order
+    product_id VARCHAR(50) REFERENCES superstore.products(product_id), -- Reference to the product in the order
+    sales FLOAT NOT NULL, -- Sales amount
+    quantity INT NOT NULL, -- Quantity of the product in the order
+    discount FLOAT NOT NULL, -- Discount applied to the order
+    profit FLOAT NOT NULL -- Profit from the order
 );
 
 -- Import data into ORDERS table from a CSV file
-COPY FIT_PRO.ORDERS (order_id, customer_id, status, checked_out_at)
-FROM 'C:\Users\HP\Downloads\orders.csv' DELIMITER ',' CSV HEADER; -- Importing order data from CSV file
+COPY superstore.orders (order_id, order_date, ship_date, ship_mode, customer_id, product_id, sales, quantity, discount, profit)
+FROM 'C:\Users\HP\Desktop\altschool_project\third_semester_repo\Postgres_Docker\postgres_docker_init\data\orders.csv' DELIMITER ',' CSV HEADER; -- Importing order data from CSV file
 
--- Create EVENTS table
-CREATE TABLE IF NOT EXISTS FIT_PRO.EVENTS
-(
-    event_id SERIAL PRIMARY KEY, -- Unique identifier for each event
-    customer_id UUID REFERENCES FIT_PRO.CUSTOMERS(customer_id), -- Reference to the customer associated with the event
-    event_data JSONB NOT NULL, -- JSONB data type for storing JSON objects representing event data
-    event_timestamp DATE NOT NULL -- Date of the event, without time information
+-- Create ARCHIVED_ORDERS table
+CREATE TABLE IF NOT EXISTS superstore.archived_orders (
+    order_id VARCHAR(50) PRIMARY KEY, -- Unique identifier for each archived order
+    order_date VARCHAR(50) NOT NULL, -- Order date
+    ship_date VARCHAR(50) NOT NULL, -- Shipping date
+    ship_mode VARCHAR(50) NOT NULL, -- Shipping mode
+    customer_id VARCHAR(50) REFERENCES superstore.customers(customer_id), -- Reference to the customer who placed the order
+    product_id VARCHAR(50) REFERENCES superstore.products(product_id), -- Reference to the product in the order
+    sales FLOAT NOT NULL, -- Sales amount
+    quantity INT NOT NULL, -- Quantity of the product in the order
+    discount FLOAT NOT NULL, -- Discount applied to the order
+    profit FLOAT NOT NULL -- Profit from the order
 );
 
--- Import data into EVENTS table from a CSV file
-COPY FIT_PRO.EVENTS (event_id, customer_id, event_data, event_timestamp)
-FROM 'C:\Users\HP\Downloads\events.csv' DELIMITER ',' CSV HEADER; -- Importing event data from CSV file
-
--- Create LINE_ITEMS table
-CREATE TABLE IF NOT EXISTS FIT_PRO.LINE_ITEMS
-(
-    line_item_id SERIAL PRIMARY KEY, -- Unique identifier for each line item
-    order_id UUID REFERENCES FIT_PRO.ORDERS(order_id), -- Reference to the order to which the line item belongs
-    quantity INTEGER NOT NULL, -- Quantity of the product in the line item, stored as an integer
-    item_id INTEGER -- Assuming item_id is an integer; add REFERENCES if needed
-);
-
--- Import data into LINE_ITEMS table from a CSV file
-COPY ALT_SCHOOL.LINE_ITEMS (line_item_id, order_id, quantity, item_id)
-FROM 'C:\Users\HP\Downloads\line_items.csv' DELIMITER ',' CSV HEADER; -- Importing line item data from CSV file
+-- Import data into ARCHIVED_ORDERS table from a CSV file
+COPY superstore.archived_orders (order_id, order_date, ship_date, ship_mode, customer_id, product_id, sales, quantity, discount, profit)
+FROM 'C:\Users\HP\Desktop\altschool_project\third_semester_repo\Postgres_Docker\postgres_docker_init\data\archived_orders.csv' DELIMITER ',' CSV HEADER; -- Importing archived order data from CSV file
